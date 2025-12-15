5b3404a36ee2662f60b4427816cac9893aa853b7
Ashutosh Kumar <130897584+codeaashu@users.noreply.github.com>
2025-12-15 23:32:24 +0530

    i love icecream

---
+++ src/pages/text.tsx
@@ -0,0 +1,12 @@
+export default function IceCreamText() {
+    return (
+        <div style={{ textAlign: 'center', padding: '20px' }}>
+            <h1 style={{ color: 'red', fontSize: '48px', fontWeight: 'bold' }}>
+                🍦 Ice Cream 🍦
+            </h1>
+            <p style={{ color: 'red', fontSize: '24px' }}>
+                Enjoy delicious ice cream!
+            </p>
+        </div>
+    );
+}
